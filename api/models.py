00cc3c86018cfc3d55a5dad6831bddab84f2129d
Milton Lima <milton.lima@outlook.com>
2020-07-16 15:29:25 -0300

    Update models files and others documents and admin

--- api/models.py
+++ api/models.py
@@ -1,11 +1,26 @@
+
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+category_CHOICES = [
+    ('PROD','Produção'),
+    ('HOMO','Homologação'),
+    ('DEV', 'Desenvolvimento')
+]
 
-class cadastroUsuario(models.Model):
-    email = models.CharField(max_length=70, blank=False, default='')
-    password = models.CharField( min_length=12, blank=False, default='' )
 
-class login(models.Model):
-    email = models.CharField(max_length=70, blank=False,required=True,  default='')
-    password = models.CharField( min_length=12, blank=False, required=True, default='' )
+class User(models.Model):
+    name = models.CharField("Name", max_length=100)
+    email = models.CharField("E-mail")
+    password = models.CharField("Password", max_length=18, validators = [MinLengthValidator])
+
+    def __str__(self):
+        return self.name
+        
+
+class LogEntry(models.Model):
+    title = models.CharField(max_length=200)
+    createdAt = models.DateTimeField
+    updatedAt = models.DateTimeField
+    isAArchived = models.BooleanField
+    category = [category_CHOICES]
