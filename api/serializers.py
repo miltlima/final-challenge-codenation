9e0e36138a69c48bc31d7e37c83e0cd5c550956b
Milton Lima <milton.lima@outlook.com>
2020-07-16 21:59:26 -0300

    Adicionado ID ao serializers para teste

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,7 @@ from api.models import User, LogEntry
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('name', 'email')
+        fields = ('name', 'email', 'id')
 
 class LogEntrySerializer(ModelSerializer):
     class Meta:
